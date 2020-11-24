27cdbc4461e884f3ee759a3098e89ba28c6c5e11
z3t4z00k <z3t4z00k@gmail.com>
2020-11-24 01:14:18 -0500

    Created color scheme

---
+++ constants/colors.js
@@ -0,0 +1,16 @@
+export default Colors = {
+	white: '#fdfdfd',
+	gray: '#a0a0a0',
+	black: '#101010',
+	accent: '#F9474E',
+
+	emot1: '#FF5252',
+	emot2: '#536dfe',
+	emot3: '#7c4dff',
+	emot4: '#757575',
+	emot5: '#ffab40',
+	emot6: '#ffffff',
+	emot7: '#fdd835',
+	emot8: '#cddc39',
+	emot9: '#ff4081'
+}
