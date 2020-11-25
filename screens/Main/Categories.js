924cb9c1599d5e77a974e4bd29fad88b6d50878d
z3t4z00k <z3t4z00k@gmail.com>
2020-11-25 11:43:26 -0500

    Created various screens

---
+++ screens/Main/Categories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+import Heading from '../../components/Typo/Heading'
+
+const Categories = props => {
+	return (
+		<View>
+			<Heading lvl={1} text="Categories Screen"/>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Categories
---
+++ screens/Main/History.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+import Heading from '../../components/Typo/Heading'
+
+const History = props => {
+	return (
+		<View>
+			<Heading lvl={1} text="History Screen"/>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default History
