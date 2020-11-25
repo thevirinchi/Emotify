05e081f72c6bf857a9bc78142b064010cb1ac191
z3t4z00k <z3t4z00k@gmail.com>
2020-11-25 07:00:05 -0500

    Added third onboarding screen

--- navigation/OnBoardingNavigator.js
+++ navigation/OnBoardingNavigator.js
@@ -5,10 +5,12 @@ import { createAppContainer } from 'react-navigation'
 
 import OnBoarding1 from '../screens/OnBoarding/One'
 import OnBoarding2 from '../screens/OnBoarding/Two'
+import OnBoarding3 from '../screens/OnBoarding/Three'
 
 const OnBoardingNavigator = createStackNavigator({
 	onBoardingScreen1: OnBoarding1,
-	onBoardingScreen2: OnBoarding2
+	onBoardingScreen2: OnBoarding2,
+	onBoardingScreen3: OnBoarding3
 })
 
 export default createAppContainer(OnBoardingNavigator)
