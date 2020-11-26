0f2212684c8200ebe3289273d8aec60f01f9b471
z3t4z00k <z3t4z00k@gmail.com>
2020-11-26 07:34:58 -0500

    Created data models for categories and songs

---
+++ models/Song.js
@@ -0,0 +1,10 @@
+class SongModel {
+	constructor(id, icon, title, authors) {
+		this.id = id;
+		this.icon = icon;
+		this.title = title;
+		this.authors = authors;
+	}
+}
+
+export default SongModel;
