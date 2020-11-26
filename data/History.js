cc6ee9b055e04b1f1178434fac426d5426dcd4de
z3t4z00k <z3t4z00k@gmail.com>
2020-11-26 11:51:04 -0500

    Added sample history data

---
+++ data/History.js
@@ -0,0 +1,19 @@
+import SongModel from '../models/Song'
+
+export const HistoryData = [
+	new SongModel('s1', "1", 'Mood (feat. iann dior)', '24kGoldn, Iann Dior'),
+	new SongModel('s2', "2", 'Lemonade', 'Internet Money, Gunna, Don Toliver, NAV'),
+	new SongModel('s3', "3", 'Hawai', 'Maluma'),
+	new SongModel('s4', "4", 'Dynamite', 'BTS'),
+	new SongModel('s5', "5", 'Blinding Lights', 'The Weeknd'),
+	new SongModel('s6', "1", 'Mood (feat. iann dior)', '24kGoldn, Iann Dior'),
+	new SongModel('s7', "2", 'Lemonade', 'Internet Money, Gunna, Don Toliver, NAV'),
+	new SongModel('s8', "3", 'Hawai', 'Maluma'),
+	new SongModel('s9', "4", 'Dynamite', 'BTS'),
+	new SongModel('s10', "5", 'Blinding Lights', 'The Weeknd'),
+	new SongModel('s11', "1", 'Mood (feat. iann dior)', '24kGoldn, Iann Dior'),
+	new SongModel('s12', "2", 'Lemonade', 'Internet Money, Gunna, Don Toliver, NAV'),
+	new SongModel('s13', "3", 'Hawai', 'Maluma'),
+	new SongModel('s14', "4", 'Dynamite', 'BTS'),
+	new SongModel('s15', "5", 'Blinding Lights', 'The Weeknd')
+]
