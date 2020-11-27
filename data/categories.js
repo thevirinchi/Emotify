3fbff99a84ea03ebd6daa3d29777027df9515c53
z3t4z00k <z3t4z00k@gmail.com>
2020-11-27 14:42:25 -0500

    Fixed colors

--- data/categories.js
+++ data/categories.js
@@ -5,8 +5,8 @@ import CategoryModel from '../models/Category'
 
 export const CategoriesData = [
 	new CategoryModel('c1', "EmoteAngry", 'Angry', Colors.emot1, Colors.white),
-	new CategoryModel('c2', "EmoteGrinning", 'Happy', Colors.emot5, Colors.white),
-	new CategoryModel('c3', "EmoteNoExpression", 'Neutral', Colors.emot2, Colors.White),
+	new CategoryModel('c2', "EmoteGrinning", 'Happy', Colors.emot5, Colors.black),
+	new CategoryModel('c3', "EmoteNoExpression", 'Neutral', Colors.emot2, Colors.white),
 	new CategoryModel('c4', "EmoteSad", 'Sad', Colors.emot4, Colors.white),
 	new CategoryModel('c5', "EmoteStars", 'Surprise', Colors.emot3, Colors.white)
 ]
