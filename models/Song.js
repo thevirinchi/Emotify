f64dd08ac9951f04541a1b2952f7566a592f32bf
z3t4z00k <z3t4z00k@gmail.com>
2020-11-27 15:28:46 -0500

    Added category id

--- models/Song.js
+++ models/Song.js
@@ -1,6 +1,7 @@
 class SongModel {
-	constructor(id, icon, title, authors) {
+	constructor(id, catId, icon, title, authors) {
 		this.id = id;
+		this.catId = catId
 		this.icon = icon;
 		this.title = title;
 		this.authors = authors;
