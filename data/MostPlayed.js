fd1e20a9afe23898732442163efa30a3ee90f155
z3t4z00k <z3t4z00k@gmail.com>
2020-11-27 15:52:56 -0500

    Changing Data Strcuture

--- data/MostPlayed.js
+++ data/MostPlayed.js
@@ -1,9 +1,9 @@
 import SongModel from '../models/Song'
 
 export const MostPlayed = [
-	new SongModel('s1', "1", 'Mood (feat. iann dior)', '24kGoldn, Iann Dior'),
-	new SongModel('s2', "2", 'Lemonade', 'Internet Money, Gunna, Don Toliver, NAV'),
-	new SongModel('s3', "3", 'Hawai', 'Maluma'),
-	new SongModel('s4', "4", 'Dynamite', 'BTS'),
-	new SongModel('s5', "5", 'Blinding Lights', 'The Weeknd')
+	new SongModel('s1', "c", "1", 'Mood (feat. iann dior)', '24kGoldn, Iann Dior'),
+	new SongModel('s2', "c", "2", 'Lemonade', 'Internet Money, Gunna, Don Toliver, NAV'),
+	new SongModel('s3', "c", "3", 'Hawai', 'Maluma'),
+	new SongModel('s4', "c", "4", 'Dynamite', 'BTS'),
+	new SongModel('s5', "c", "5", 'Blinding Lights', 'The Weeknd'),
 ]
--- data/SuggestionData.js
+++ data/SuggestionData.js
@@ -1,14 +1,14 @@
 import SongModel from '../models/Song'
 
 export const SuggestionData = [
-	new SongModel('s1', "1", 'Mood (feat. iann dior)', '24kGoldn, Iann Dior'),
-	new SongModel('s2', "2", 'Lemonade', 'Internet Money, Gunna, Don Toliver, NAV'),
-	new SongModel('s3', "3", 'Hawai', 'Maluma'),
-	new SongModel('s4', "4", 'Dynamite', 'BTS'),
-	new SongModel('s5', "5", 'Blinding Lights', 'The Weeknd'),
-	new SongModel('s6', "1", 'Mood (feat. iann dior)', '24kGoldn, Iann Dior'),
-	new SongModel('s7', "2", 'Lemonade', 'Internet Money, Gunna, Don Toliver, NAV'),
-	new SongModel('s8', "3", 'Hawai', 'Maluma'),
-	new SongModel('s9', "4", 'Dynamite', 'BTS'),
-	new SongModel('s10', "5", 'Blinding Lights', 'The Weeknd')
+	new SongModel('s6', "c1", "6", 'Alone - Sullivan King Remix', 'Kayzo, Our Last Night, Sullivan King'),
+	new SongModel('s7', "c1", "7", 'Invincible', 'Skellism, One Shot Thrill'),
+	new SongModel('s8', "c1", "8", 'Lake Of Fire', 'Ghost Stories, D-Block & S-te-Fan'),
+	new SongModel('s9', "c1", "9", 'How I Like It', 'VINAI'),
+	new SongModel('s10', "c1", "10", 'Believer - Frontliner Remix', 'Major Lazor, Showtek, Frontliner'),
+	new SongModel('s11', "c1", "11", '200 BPM', 'Frontliner'),
+	new SongModel('s12', "c1", "12", 'Feel It!', 'D-Block & S-te-Fan, D-Sturb'),
+	new SongModel('s13', "c1", "13", 'Vienna', 'Will Spark'),
+	new SongModel('s14', "c1", "14", 'Arriba', 'Psyko Punkz, Coone, KELTEK'),
+	new SongModel('s15', "c1", "15", 'Gabry Ponte', 'La Diva')
 ]
