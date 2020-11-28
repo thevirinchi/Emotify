353e3bde30ac9244b38cdb05ab41e0a781f88c5b
z3t4z00k <z3t4z00k@gmail.com>
2020-11-28 00:31:59 -0500

    Updated most played songs data structure

--- data/MostPlayed.js
+++ data/MostPlayed.js
@@ -1,9 +1,9 @@
 import SongModel from '../models/Song'
 
 export const MostPlayed = [
-	new SongModel('s1', "c", "1", 'Mood (feat. iann dior)', '24kGoldn, Iann Dior'),
-	new SongModel('s2', "c", "2", 'Lemonade', 'Internet Money, Gunna, Don Toliver, NAV'),
-	new SongModel('s3', "c", "3", 'Hawai', 'Maluma'),
-	new SongModel('s4', "c", "4", 'Dynamite', 'BTS'),
-	new SongModel('s5', "c", "5", 'Blinding Lights', 'The Weeknd'),
+	new SongModel('s1', 'c1', "https://i.scdn.co/image/ab67616d00004851a9142ec2ee4c9636a7c525c6", "Alone - Sullivan King Remix",  "Kayzo, Our Last Night, Sullivan King"),
+	new SongModel('s11', 'c2', "https://i.scdn.co/image/ab67616d000048514c782935ead3b4bfccc946ae", "The Motown Song", "Red Stewart, The Temptations"),
+	new SongModel('s21', 'c3', "https://i.scdn.co/image/ab67616d0000485149e64dd5554e84bed33e6ea2", "Prey", "Lehiotik"),
+	new SongModel('s30', 'c5', "https://i.scdn.co/image/ab67616d000048510dd46cd9d28e2980f633f409", "Mi Sento Bene", "Simple Lo-Fi, Ney Marques"),
+	new SongModel('s41', 'c4', "https://i.scdn.co/image/ab67616d00004851cdebae610076f51e8b7b20f4", "Skinny Love", "Birdy"),
 ]
