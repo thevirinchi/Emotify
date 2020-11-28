df6f7c442f6e26b14bbee2200cb54e717e0b7d26
z3t4z00k <z3t4z00k@gmail.com>
2020-11-28 00:01:13 -0500

    Added data into models

--- data/Songs.js
+++ data/Songs.js
@@ -1,55 +1,54 @@
--------- Party ---------
-https://i.scdn.co/image/ab67616d0000485191e93c59bacfe819db9601eb - Head & Heart (feat. MNEK) [Joel Corry, MNEK]
-https://i.scdn.co/image/ab67616d000048517e957714488104947de6a5e8 - I want it all [Lucas & Steve]
-https://i.scdn.co/image/ab67616d00004851add90b718986f94ab896abe1 - Same Star [Thierry Von Der Warth, Jordan Jade]
-https://i.scdn.co/image/ab67616d00004851aaef72a18979fbc8d5aa4a65 - Lovefool (feat. Pia Mila) [twocolors, Pia Mila]
-https://i.scdn.co/image/ab67616d00004851da74fdbb0c6c12d85dd9afd7 - Call Me Up [Braaheim] 
-https://i.scdn.co/image/ab67616d000048519080eb7823cc4b25a97d4257 - Dont Waste my time [Thierry Von Der Warth]
-https://i.scdn.co/image/ab67616d00004851c65af91af9f9290c2b13fe59 - Surrender [Kenn Colt, Ashton Love]
-https://i.scdn.co/image/ab67616d00004851cb1f21b3197199d74d838ea0 - pumped up kicks [Madism, MKJ, Felix Samuel]
-https://i.scdn.co/image/ab67616d00004851939faa21654a2c65c71f55a2 - Nobody [NOTD, Catello]
-https://i.scdn.co/image/ab67616d00004851ebafa7f05f414679c41b0de5 - Head Shoulders Knees & Toes [Ofenbach, Quarterhead, Norma Jean Martine]
--------- RAGE ---------
-https://i.scdn.co/image/ab67616d00004851a9142ec2ee4c9636a7c525c6 - Alone - Sullivan King Remix [Kayzo, Our Last Night, Sullivan King]
-https://i.scdn.co/image/ab67616d000048510f8c4a417755e44f804147cc - Invincible [Skellism, One Shot Thrill]
-https://i.scdn.co/image/ab67616d000048517345010c5a6ea8669df1e24d - Lake Of Fire [Ghost Stories, D-Black & S-te-Fan]
-https://i.scdn.co/image/ab67616d00004851a9732b9c49e0b63a7f9de9dc - How I Like It [VINAI]
-https://i.scdn.co/image/ab67616d00004851860ef1cfc8f6a5d76634d0b6 - Believer - Frontliner Remix [Major Lazor, Showtek, FrontLiner]
-https://i.scdn.co/image/ab67616d000048511dc868e02698ff41b4f97e4d - 200 BPM [Frontliner]
-https://i.scdn.co/image/ab67616d00004851a1e7091ae67b856fc9060c34 - Feel It! [D-Block & S-te-Fan, D-Sturb]
-https://i.scdn.co/image/ab67616d00004851cc06d91c675f786a30790ad3 - Vienna [Will Sparks]
-https://i.scdn.co/image/ab67616d00004851c7cd91ffc4b21958678bb742 - Arriba [Psyko Punkz, Coone, KELTEK] 
-https://i.scdn.co/image/ab67616d0000485126ae72fabe581d5831c65a24 - Opera [Gabry Ponte, La Diva]
--------- Happy ---------
-https://i.scdn.co/image/ab67616d000048514c782935ead3b4bfccc946ae - The Motown Song [Red Stewart, The Temptations]
-https://i.scdn.co/image/ab67616d00004851bf6cc1130a676e73730f6311 - Gonna Make You Sweat [C & C Music Factory]
-https://i.scdn.co/image/ab67616d00004851ab7b495554c93a381338691c - JoyRide - Single Version [Roxette]
-https://i.scdn.co/image/ab67616d00004851e16a6534cd5a968a5b725d03 - Dancing in the moonlight [Toploader]
-https://i.scdn.co/image/ab67616d00004851574814e86814f3540f2aadb2 - Moving On Up [M People]
-https://i.scdn.co/image/ab67616d000048518720ccf30a96ef19222ce16a - I wanna be the only one [Eternal, Babe Winans]
-https://i.scdn.co/image/ab67616d0000485105a41288f037a08fb45db5e2 - Cant stop this thing We started [Bryan Adams]
-https://i.scdn.co/image/ab67616d0000485184fef39a9a8293c86796599f - I want you [Savage Garden]
-https://i.scdn.co/image/ab67616d000048512aa20611c7fb964a74ab01a6 - Lucky [Britney Spears]
-https://i.scdn.co/image/ab67616d000048515320a1b471ae75632ef787e5 - Breathless [The Corrs]
---------- SAD ----------
-https://i.scdn.co/image/ab67616d00004851cdebae610076f51e8b7b20f4 - Skinny Love [Birdy]
-https://i.scdn.co/image/ab67616d000048511ad149489625bd3b7d9ea6a2 - Lost Boy [Ruth B.]
-https://i.scdn.co/image/ab67616d0000485186734c2b36be7adcaa4ab1f0 - One Day [Tete McRae]
-https://i.scdn.co/image/ab67616d000048516cf75f2a756e1eb6e279c700 - Before you go - piano version  [Lewis Capaldi]
-https://i.scdn.co/image/ab67616d00004851b280aea74eef855675d7b93b - Someone you loved [Lewis Capaldi]
-https://i.scdn.co/image/ab67616d00004851b280aea74eef855675d7b93b - you are the reason [Calum Scott]
-https://i.scdn.co/image/ab67616d000048510a64e2680bbc44107022e326 - Get you the moon (feat. Snow) [Kina, Snow]
-https://i.scdn.co/image/ab67616d0000485137434b17690c99a7d764502e - Sunlight [Roundrobin, Marlus]
-https://i.scdn.co/image/ab67616d00004851d841ad4d4aa791514d7e9758 - you were good to me [Jeremy Zucker, Chelsea Cutler]
-https://i.scdn.co/image/ab67616d00004851005cd7d0ae87b081601f6cca - too good at goodbye [Sam Smith]
-------- Neutral ---------
-https://i.scdn.co/image/ab67616d0000485149e64dd5554e84bed33e6ea2  - Prey [Lehiotik]
-https://i.scdn.co/image/ab67616d000048517223401f3cf85b44ff44ce8b - I Dont Care [aesvec.]
-https://i.scdn.co/image/ab67616d00004851612e631f2db4b14a5a142052 - Crush [braj mahal]
-https://i.scdn.co/image/ab67616d000048515ddaa7ddf9063a9909ce029f - wish we could stay like this forever [ocha]
-https://i.scdn.co/image/ab67616d00004851d298040fad11781f5f07add2 - sweet hangover [Peter Kind, Platinum Dust]
-https://i.scdn.co/image/ab67616d0000485109caafd59ac3e8ceb4ff7a0b - mindfulness [Tohaj]
-https://i.scdn.co/image/ab67616d00004851987f6c8b0a11c1d307381ceb - going home [Dualizm, Grmmr.126]
-https://i.scdn.co/image/ab67616d000048515cdc0f8db106122103a469e4 - Chillin' [twuan]
-https://i.scdn.co/image/ab67616d0000485151378e53360705e3eb7a8335 - city lounge [Saib]
-https://i.scdn.co/image/ab67616d000048510dd46cd9d28e2980f633f409 - mi sento bene [Simple Lo-Fi, Ney Marques]
+import SongModel from '../models/Song'
+
+export default SongsData = [
+	new SongModel('s1', 'c1', "https://i.scdn.co/image/ab67616d00004851a9142ec2ee4c9636a7c525c6", "Alone - Sullivan King Remix",  "Kayzo, Our Last Night, Sullivan King"),
+	new SongModel('s2', 'c1', "https://i.scdn.co/image/ab67616d000048510f8c4a417755e44f804147cc", "Invincible", "Skellism, One Shot Thrill"),
+	new SongModel('s3', 'c1', "https://i.scdn.co/image/ab67616d000048517345010c5a6ea8669df1e24d", "Lake Of Fire", "Ghost Stories, D-Black & S-te-Fan"),
+	new SongModel('s4', 'c1', "https://i.scdn.co/image/ab67616d00004851a9732b9c49e0b63a7f9de9dc", "How I Like It", "VINAI"),
+	new SongModel('s5', 'c1', "https://i.scdn.co/image/ab67616d00004851860ef1cfc8f6a5d76634d0b6", "Believer - Frontliner Remix", "Major Lazor, Showtek, FrontLiner"),
+	new SongModel('s6', 'c1', "https://i.scdn.co/image/ab67616d000048511dc868e02698ff41b4f97e4d", "200 BPM", "Frontliner"),
+	new SongModel('s7', 'c1', "https://i.scdn.co/image/ab67616d00004851a1e7091ae67b856fc9060c34", "Feel It!", "D-Block & S-te-Fan, D-Sturb"),
+	new SongModel('s8', 'c1', "https://i.scdn.co/image/ab67616d00004851cc06d91c675f786a30790ad3", "Vienna", "Will Sparks"),
+	new SongModel('s9', 'c1', "https://i.scdn.co/image/ab67616d00004851c7cd91ffc4b21958678bb742", "Arriba", "Psyko Punkz, Coone, KELTEK"),
+	new SongModel('s10', 'c1', "https://i.scdn.co/image/ab67616d0000485126ae72fabe581d5831c65a24", "Opera", "Gabry Ponte, La Diva"),
+	new SongModel('s11', 'c2', "https://i.scdn.co/image/ab67616d000048514c782935ead3b4bfccc946ae", "The Motown Song", "Red Stewart, The Temptations"),
+	new SongModel('s12', 'c2', "https://i.scdn.co/image/ab67616d00004851bf6cc1130a676e73730f6311", "Gonna Make You Sweat", "C & C Music Factory"),
+	new SongModel('s13', 'c2', "https://i.scdn.co/image/ab67616d00004851ab7b495554c93a381338691c", "JoyRide - Single Version", "Roxette"),
+	new SongModel('s14', 'c2', "https://i.scdn.co/image/ab67616d00004851e16a6534cd5a968a5b725d03", "Dancing in the moonlight", "Toploader"),
+	new SongModel('s15', 'c2', "https://i.scdn.co/image/ab67616d00004851574814e86814f3540f2aadb2", "Moving On Up", "M People"),
+	new SongModel('s16', 'c2', "https://i.scdn.co/image/ab67616d000048518720ccf30a96ef19222ce16a", "I wanna be the only one", "Eternal, Babe Winans"),
+	new SongModel('s17', 'c2', "https://i.scdn.co/image/ab67616d0000485105a41288f037a08fb45db5e2", "Cant stop this thing We started", "Bryan Adams"),
+	new SongModel('s18', 'c2', "https://i.scdn.co/image/ab67616d0000485184fef39a9a8293c86796599f", "I want you", "Savage Garden"),
+	new SongModel('s19', 'c2', "https://i.scdn.co/image/ab67616d000048512aa20611c7fb964a74ab01a6", "Lucky", "Britney Spears"),
+	new SongModel('s20', 'c2', "https://i.scdn.co/image/ab67616d000048515320a1b471ae75632ef787e5", "Breathless", "The Corrs"),
+	new SongModel('s21', 'c3', "https://i.scdn.co/image/ab67616d0000485149e64dd5554e84bed33e6ea2", "Prey", "Lehiotik"),
+	new SongModel('s22', 'c3', "https://i.scdn.co/image/ab67616d000048517223401f3cf85b44ff44ce8b", "I Dont Care", "aesvec."),
+	new SongModel('s23', 'c3', "https://i.scdn.co/image/ab67616d00004851612e631f2db4b14a5a142052", "Crush", "braj mahal"),
+	new SongModel('s24', 'c3', "https://i.scdn.co/image/ab67616d000048515ddaa7ddf9063a9909ce029f", "Wish We Could Stay Like This Forever", "ocha"),
+	new SongModel('s25', 'c3', "https://i.scdn.co/image/ab67616d00004851d298040fad11781f5f07add2", "Sweet Hangover", "Peter Kind, Platinum Dust"),
+	new SongModel('s26', 'c3', "https://i.scdn.co/image/ab67616d0000485109caafd59ac3e8ceb4ff7a0b", "Mindfulness", "Tohaj"),
+	new SongModel('s27', 'c3', "https://i.scdn.co/image/ab67616d00004851987f6c8b0a11c1d307381ceb", "Going Home", "Dualizm, Grmmr.126"),
+	new SongModel('s28', 'c3', "https://i.scdn.co/image/ab67616d000048515cdc0f8db106122103a469e4", "Chillin'", "twuan"),
+	new SongModel('s29', 'c3', "https://i.scdn.co/image/ab67616d0000485151378e53360705e3eb7a8335", "City Lounge", "Saib"),
+	new SongModel('s30', 'c3', "https://i.scdn.co/image/ab67616d000048510dd46cd9d28e2980f633f409", "Mi Sento Bene", "Simple Lo-Fi, Ney Marques"),
+	new SongModel('s31', 'c4', "https://i.scdn.co/image/ab67616d0000485191e93c59bacfe819db9601eb", "Head & Heart (feat. MNEK)", "Joel Corry, MNEK"),
+	new SongModel('s32', 'c4', "https://i.scdn.co/image/ab67616d000048517e957714488104947de6a5e8", "I want It All", "Lucas & Steve"),
+	new SongModel('s33', 'c4', "https://i.scdn.co/image/ab67616d00004851add90b718986f94ab896abe1", "Same Star", " Thierry Von Der Warth, Jordan Jade"),
+	new SongModel('s34', 'c4', "https://i.scdn.co/image/ab67616d00004851aaef72a18979fbc8d5aa4a65", "Lovefool (feat. Pia Mila)", "Twocolors, Pia Mila"),
+	new SongModel('s35', 'c4', "https://i.scdn.co/image/ab67616d00004851da74fdbb0c6c12d85dd9afd7", "Call Me Up", "Braaheim"),
+	new SongModel('s36', 'c4', "https://i.scdn.co/image/ab67616d000048519080eb7823cc4b25a97d4257", "Dont Waste my time", "Thierry Von Der Warth"),
+	new SongModel('s37', 'c4', "https://i.scdn.co/image/ab67616d00004851c65af91af9f9290c2b13fe59", "Surrender", " Kenn Colt, Ashton Love"),
+	new SongModel('s38', 'c4', "https://i.scdn.co/image/ab67616d00004851cb1f21b3197199d74d838ea0", "Pumoed Up Kicks", "Madism, MKJ, Felix Samuel"),
+	new SongModel('s39', 'c4', "https://i.scdn.co/image/ab67616d00004851939faa21654a2c65c71f55a2", "Nobody", "NOTD, Catello"),
+	new SongModel('s40', 'c4', "https://i.scdn.co/image/ab67616d00004851ebafa7f05f414679c41b0de5", "Head Shoulders Knees & Toes", "Ofenbach, Quarterhead, Norma Jean Martine"),
+	new SongModel('s41', 'c5', "https://i.scdn.co/image/ab67616d00004851cdebae610076f51e8b7b20f4", "Skinny Love", "Birdy"),
+	new SongModel('s42', 'c5', "https://i.scdn.co/image/ab67616d000048511ad149489625bd3b7d9ea6a2", "Lost Boy", "Ruth B."),
+	new SongModel('s43', 'c5', "https://i.scdn.co/image/ab67616d0000485186734c2b36be7adcaa4ab1f0", "One Day", "Tete McRae"),
+	new SongModel('s44', 'c5', "https://i.scdn.co/image/ab67616d000048516cf75f2a756e1eb6e279c700", "Before You Go - Piano Version", "Lewis Capaldi"),
+	new SongModel('s45', 'c5', "https://i.scdn.co/image/ab67616d00004851b280aea74eef855675d7b93b", "Someone you loved", "Lewis Capaldi"),
+	new SongModel('s46', 'c5', "https://i.scdn.co/image/ab67616d00004851b280aea74eef855675d7b93b", "You Are The Reason", "Calum Scott"),
+	new SongModel('s47', 'c5', "https://i.scdn.co/image/ab67616d000048510a64e2680bbc44107022e326", "Get you the moon (feat. Snow)", "Kina, Snow"),
+	new SongModel('s48', 'c5', "https://i.scdn.co/image/ab67616d0000485137434b17690c99a7d764502e", "Sunlight", "Roundrobin, Marlus"),
+	new SongModel('s49', 'c5', "https://i.scdn.co/image/ab67616d00004851d841ad4d4aa791514d7e9758", "You Were Good To Me", " Jeremy Zucker, Chelsea Cutler"),
+	new SongModel('s50', 'c5', "https://i.scdn.co/image/ab67616d00004851005cd7d0ae87b081601f6cca", "Too Good At Goodbye", "Sam Smith"),
+]
