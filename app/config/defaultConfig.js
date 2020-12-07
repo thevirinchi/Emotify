cb5b3b281d8694a8760579a8dd8428ef0729d3b4
z3t4z00k <z3t4z00k@gmail.com>
2020-12-07 01:21:30 -0500

    Restructured project

---
+++ app/config/defaultConfig.js
@@ -0,0 +1,1 @@
+export const FIREBASE_API_KEY = "AIzaSyC5IBjvLYBfU6laOtx6VDMKmmdFwrMKa6k "
---
+++ app/config/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	apiKey: "AIzaSyC5IBjvLYBfU6laOtx6VDMKmmdFwrMKa6k",
+	authDomain: "emotify-139d5.firebaseapp.com",
+	databaseURL: "https://emotify-139d5.firebaseio.com",
+	projectId: "emotify-139d5",
+	storageBucket: "emotify-139d5.appspot.com",
+	messagingSenderId: "484146853561",
+	appId: "1:484146853561:web:e4a0b974fa82f265274d68"
+};
