b4a85e23dc65a0c08efb04e34b8e0787487c1690
z3t4z00k <z3t4z00k@gmail.com>
2020-12-07 01:29:13 -0500

    Created express app

---
+++ app.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios')
+const cors = require('cors');
+
+/*	PRODUCTION	*/
+// var key = fs.readFileSync(__dirname + '/server.key');
+// var cert = fs.readFileSync(__dirname + '/server.crt');
+// var ca = fs.readFileSync(__dirname + '/ca_bundle');
+// var options = {
+//     key: key,
+//     cert: cert,
+//     ca: ca,
+// };
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/status', (req, res) => res.send('Working!'));
+
+/*  PRODUCTION  */
+// var server = https.createServer(options, app);
+// server.listen(443, () => {
+//     console.log("server starting on port : " + 443)
+// });
+
+/*  DEVELOPMENT */
+app.set('port', process.env.PORT || 8080);
+app.listen(8080);
