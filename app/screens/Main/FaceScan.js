090f6e51fcd0d43ccc85bd973409a08fdb8b3bc2
z3t4z00k <z3t4z00k@gmail.com>
2020-12-08 03:16:31 -0500

    Added API call to send image from camera to Express API for FER

--- app/screens/Main/FaceScan.js
+++ app/screens/Main/FaceScan.js
@@ -3,6 +3,8 @@ import { View, Text, StyleSheet, Image, Dimensions, ScrollView, Alert } from 're
 import { FlatList, TouchableNativeFeedback } from 'react-native-gesture-handler'
 import * as ImagePicker from 'expo-image-picker'
 import * as Permissions from 'expo-permissions'
+import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
 
 import Heading from '../../components/Typo/Heading'
 import Display from '../../components/Typo/Display'
@@ -17,9 +19,13 @@ import Button from '../../components/Button/Button'
 import { CategoriesData } from '../../data/categories'
 import { SongsData } from '../../data/Songs'
 
+import * as loadingActions from '../../store/action/spinner'
+
 const FaceScan = props => {
 
-	const catId = 'c2'
+	const dispatch = useDispatch()
+
+	const [catId, setCatId] = useState('c2')
 	const category = CategoriesData.find(category => category.id === catId)
 	const songSuggestions = SongsData.filter(song => song.catId === catId)
 
@@ -40,6 +46,10 @@ const FaceScan = props => {
 		setData(songSuggestions.slice(0, suggestionCount))
 	}, [suggestionCount])
 
+	useEffect(() => {
+		console.log({catId})
+	}, [catId])
+
 	useEffect(async()=>{
 		if(scanMode){
 			const hasPermission = await verifyPermissions()
@@ -51,8 +61,34 @@ const FaceScan = props => {
 				})
 				if(image.cancelled === true)
 					props.navigation.navigate('HomeScreen')
-				else
-					console.log(image.uri)
+				else{
+					dispatch(loadingActions.toggleLoading(true, 'default', "Uploading Image..."))
+					setTimeout(function () {
+						dispatch(loadingActions.toggleLoading(true, 'default', "Running Facial Expression Recognition..."))
+					}, 3000)
+					var data = new FormData()
+					data.append('image',{
+						uri: image.uri,
+						name: "test",
+						type: 'image/jpg'
+					})
+					axios({
+						url: 'http://287030a015f7.ngrok.io/emotion',
+						method: 'post',
+						data: data,
+					}).then(res => {
+						dispatch(loadingActions.toggleLoading(false))
+						console.log(res.data)
+						if(res.data.error === true)
+							Alert.alert("Uh Oh!", "There was an error. [ " + res.data.message + " ]", [{text: "Okay", style: 'default'}])
+						else
+							setCatId(res.data.emotion)
+					}).catch(err => {
+						dispatch(loadingActions.toggleLoading(false))
+						Alert.alert("Uh Oh!", "There was an error. [ " + err.message + " ]", [{text: "Okay", style: 'default'}])
+						console.log({err})
+					})
+				}
 			}
 		}
 	}, [scanMode])
@@ -86,6 +122,9 @@ const FaceScan = props => {
 				<FlatList numColumns={1} renderItem={renderSuggestionItem} data={data} keyExtractor={item => item.id} width={"100%"} />
 				<TouchableNativeFeedback onPress={suggestionCountHandler} style={{ marginBottom: Margin.l }}>
 					<Button lvl={2} text={suggestionCount === 5 ? "Show More" : "Show Less"} style={{ backgroundColor: category.bgColor, paddingVertical: Padding.s }} containerStyle={{ marginTop: Margin.m }} />
+				</TouchableNativeFeedback>
+				<TouchableNativeFeedback onPress={()=>{toggleScanMode(true)}} style={{ marginBottom: Margin.l }}>
+					<Button lvl={2} text="Scan Again" style={{ backgroundColor: category.bgColor, paddingVertical: Padding.s }} containerStyle={{ marginTop: Margin.m }} />
 				</TouchableNativeFeedback>
 			</View>
 		</ScrollView>
